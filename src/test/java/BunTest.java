5bfc2e59c24f8348c19b1f8ff62834a61e00c611
TatianaKydryashova <kudryashova_ts@mail.ru>
2022-06-01 18:22:53 +0300

    fix code 2

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,21 +1,35 @@
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
 
 
+@RunWith(Parameterized.class)
 public class BunTest {
 
-    Bun bun = new Bun("Флюоресцентная булка R2-D3", 988);
+    private final String name;
+    private final float price;
 
-    @Test
-    public void getName() {
-        assertEquals("Название булочки не совпадает.","Флюоресцентная булка R2-D3", bun.getName());
+    public BunTest(String name, float price){
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getType() {
+        return new Object[][] {
+                {"Флюоресцентная булка R2-D3", 988},
+        };
     }
 
     @Test
-    public void getPrice() {
-        assertEquals("Цена булочки не совпадает.",988, bun.getPrice(), 0);
+    public void bunTest() {
+        Bun bun = new Bun( name, price);
+        assertEquals("Название булочки не совпадает.", name, bun.getName());
+        assertEquals("Цена булочки не совпадает.", price, bun.getPrice(), 0);
     }
+
 }
--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,24 +1,39 @@
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
-    @Test
-    public void getPrice() {
-        Ingredient ingredient = new Ingredient(IngredientType.FILLING, "cutlet", 100);
-        assertEquals("Цена ингредиента не совпадает.",100, ingredient.getPrice(), 0);
+
+    private final IngredientType type;
+    private final String name;
+    private final float price;
+
+    public IngredientTest(IngredientType type, String name, float price){
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getType() {
+        return new Object[][] {
+                {IngredientType.SAUCE, "hot sauce", 100},
+                {IngredientType.FILLING, "cutlet", 100},
+                {IngredientType.FILLING, "sausage", 300},
+        };
     }
 
     @Test
-    public void getName() {
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "hot sauce", 100);
-        assertEquals("Название ингридиента не совпадает", "hot sauce", ingredient.getName());
+    public void ingredientTest() {
+        Ingredient ingredient = new Ingredient(type, name, price);
+        assertEquals("Тип ингридиента не совпадает.", type, ingredient.getType());
+        assertEquals("Цена ингредиента не совпадает.", price, ingredient.getPrice(), 0);
+        assertEquals("Название ингридиента не совпадает", name, ingredient.getName());
     }
 
-    @Test
-    public void getType() {
-        Ingredient ingredient = new Ingredient(IngredientType.FILLING, "sausage", 300);
-        assertEquals("Тип ингридиента не совпадает.", IngredientType.FILLING, ingredient.getType());
-    }
 }
